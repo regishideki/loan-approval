15110e1375078601c246372103b9ea9a8ad1c85f
Regis Hideki Hattori <regis_hattori@yahoo.com.br>
2016-06-10 15:56:40 -0300

    initial commit

---
+++ src/main/java/org/camunda/bpm/getstarted/loanapproval/App.java
@@ -0,0 +1,13 @@
+package org.camunda.bpm.getstarted.loanapproval;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
